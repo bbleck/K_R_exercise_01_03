d63e8b35179736453b9df13997bcac653ee56966
bBleck <bmbleck@hotmail.com>
2019-02-06 22:19:16 -0700

    K & R 1.3 & 1.4 initial commit

--- K_R_exercise_01_03/main.c
+++ K_R_exercise_01_03/main.c
@@ -2,14 +2,54 @@
 //  main.c
 //  K_R_exercise_01_03
 //
-//  Created by Brian on 2/6/19.
+//  Created by Brian Bleck on 2/6/19.
 //  Copyright © 2019 Brian Bleck. All rights reserved.
 //
 
 #include <stdio.h>
 
+/*
+ print F - C table for fahr = 0, 20, ... , 300; floating point version
+ */
+
+void printHeading(char* typeBegin, char* typeConvert, int lower, int upper, int step);
+
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  float fahr, celsius;
+  int lower, upper, step;
+  char* typeBegin;
+  char* typeConvert;
+  
+  typeBegin = "fahrenheit";
+  typeConvert = "celsius";
+  lower = 0;
+  upper = 300;
+  step = 20;
+  
+  
+  fahr = lower;
+  
+  printHeading(typeBegin, typeConvert, lower, upper, step);
+  
+  while (fahr <= upper){
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr+=step;
+  }
+  printHeading(typeConvert, typeBegin, lower, upper, step);
+  
+  celsius = lower;
+  
+  while (celsius <= upper){
+    fahr = (9.0/5.0) * celsius + 32.0;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius+=step;
+  }
+  
+  
   return 0;
+}
+
+void printHeading(char* typeBegin, char* typeConvert, int lower, int upper, int step){
+  printf("Conversion chart for degrees %s to degrees %s\nStarting with %d and continuing to %d, in increments of %d\n", typeBegin, typeConvert, lower, upper, step);
 }
